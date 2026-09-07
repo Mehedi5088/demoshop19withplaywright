25afafdad1a26d9ecfa0a5f91194f0f0382263ff
Mehedi5088 <mhasan.egov@gmail.com>
2026-09-07 20:14:37 +0600

    from excel

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,25 +2,50 @@ import { test } from '@playwright/test';
 import { LoginPage as Login } from "../pages/LoginPage.js";
 import 'dotenv/config';
 import { loginData } from '../testData/logintest.js';
+import * as XLSX from 'xlsx';
 
 const users = Object.values(loginData);
 
-for (let i = 0; i < users.length; i++) {
+// for (let i = 0; i < users.length; i++) {
 
-    test(`Should Login - Test ${i + 1}`, async ({ page }) => {
+//     test(`Should Login - Test ${i + 1}`, async ({ page }) => {
 
-        const pages = new Login(page);
+//         const pages = new Login(page);
 
-        const credentials = users[i];
+//         const credentials = users[i];
+
+//         await pages.pageOpen();
+//         await pages.clickLoginButtonLink();
+
+//         await pages.enterEmail(credentials.email);
+//         await pages.enterPassword(credentials.password);
+
+//         await pages.clickRememberMeCheckbox();
+//         await pages.clickLoginButton();
+
+//     });
+// }
+
+const excelPath = '/home/mehedi/Downloads/test_data.xlsx';
+const workbook = XLSX.readFile(excelPath);
+const sheetName = workbook.SheetNames[0];
+
+const worksheet = workbook.Sheets[sheetName];
+
+const testData = XLSX.utils.sheet_to_json(worksheet);
+for (const data of testData) {
+
+    test(`Login test - ${data.username}`, async ({ page }) => {
+         const pages = new Login(page);
+
 
         await pages.pageOpen();
         await pages.clickLoginButtonLink();
-
-        await pages.enterEmail(credentials.email);
-        await pages.enterPassword(credentials.password);
-
+        await page.getByLabel('Email').fill(data.username);
+        await page.getByLabel('Password').fill(data.password);
         await pages.clickRememberMeCheckbox();
         await pages.clickLoginButton();
+        await page.waitForTimeout(2000);
 
     });
 }
